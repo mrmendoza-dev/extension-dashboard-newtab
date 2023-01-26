027ba6d3970d88884f58126d33627f8932d13552
Steven Mendoza <smendoza171@gmail.com>
2023-01-26 13:59:16 -0800

    test commit

---
+++ src/popup/popup.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './popup.css'
+
+const Popup = () => {
+    return (
+      <div>
+        <a href="https://chrome://extensions/" target="_blank">Chrome Extensions</a>
+      </div>
+    );
+};
+
+export default Popup;
---
+++ src/tabs/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter as Router } from "react-router-dom";
+import Dashboard from "./dash";
+import '../assets/tailwind.css'
+
+function init() {
+    const appContainer = document.createElement('div')
+    document.body.appendChild(appContainer)
+    if (!appContainer) {
+        throw new Error("Can not find AppContainer");
+    }
+    const root = createRoot(appContainer)
+    root.render(
+      <Router>
+        <Dashboard />
+      </Router>
+    );
+}
+
+init();
